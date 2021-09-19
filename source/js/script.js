04b6ec6f38d05483bf55e3a23b14d5a74fc0e10f
AntonOshurek <oshurekweb@gmail.com>
2021-09-19 08:27:33 +0200

    add check valute function

--- source/js/script.js
+++ source/js/script.js
@@ -1,33 +1,60 @@
 'use strict'
 
-//const { name } = require("browser-sync");
+const selectFirst = document.querySelector('.converter__select--first');
+const selectSecond = document.querySelector('.converter__select--second');
 
-async function getCurrencies () {
-    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
-    const data = await response.json();
-    const result = await data;
+const inputFirst = document.querySelector('.converter__input--first');
+const inputSecond = document.querySelector('.converter__input--second');
 
-    showValute(result.Valute)
-}
-getCurrencies();
+fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then(response => response.json())
+    .then(json => showValute(json))
 
 function showValute(val) {
-  const selectFirst = document.querySelector('.converter__select--first');
-  const selectSecond = document.querySelector('.converter__select--second');
 
-  console.log(val);
+    const valute = val.Valute;
+    let displayValute;
 
-  let displayValute
-
-  for (let key in val) {
-    console.log(val[key]);
-    //const valName = val[key];
+  for (let key in valute) {
+    console.log(valute[key]);
 
    displayValute += `
-    <option class="converter__select--value" value="${val[key].CharCode}" selected>${val[key].Name}</option>
+    <option class="converter__select--value" value="${valute[key].CharCode}" selected>${valute[key].Name}</option>
     `;
 
     selectFirst.innerHTML = displayValute;
     selectSecond.innerHTML = displayValute;
   }
-}
+  checkCurrentValute();
+};
+
+function checkCurrentValute() {
+  let firstValue = '';
+  let secondValue = '';
+  let firstValute = '';
+  let secondValute = '';
+
+  inputFirst.addEventListener('input', () => {
+    firstValue = inputFirst.value;
+  });
+
+  inputSecond.addEventListener('input', () => {
+    secondValue = inputSecond.value;
+  });
+
+  selectFirst.addEventListener('input', () => {
+    firstValute = selectFirst.value;
+    console.log(firstValute);
+  });
+
+  selectSecond.addEventListener('input', () => {
+    secondValute = selectSecond.value;
+    console.log(secondValute);
+  });
+
+  currencyCalculation();
+};
+
+function currencyCalculation() {
+
+};
