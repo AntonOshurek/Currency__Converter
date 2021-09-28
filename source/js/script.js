03b1ae9e2ccdd81bb57744eb08402f30a1845942
AntonOshurek <oshurekweb@gmail.com>
2021-09-28 21:40:53 +0200

    add clock js logick

--- source/js/script.js
+++ source/js/script.js
@@ -87,3 +87,26 @@ function currencyCalculation(firstValue, secondValue, firstValute, secondValute)
 };
 
 //time logick
+
+function getTime() {
+  const horse = document.querySelector('.time__item--horse');
+  const minutes = document.querySelector('.time__item--minutes');
+  const seconds = document.querySelector('.time__item--seconds');
+
+  function getZero (num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+  };
+
+  function updateclock() {
+    horse.innerHTML = getZero(new Date().getHours());
+    minutes.innerHTML = getZero(new Date().getMinutes());
+    seconds.innerHTML = getZero(new Date().getSeconds());
+  };
+  updateclock();
+  const timeInterva = setInterval(updateclock, 1000);
+};
+getTime();
