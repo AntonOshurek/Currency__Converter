20c97ea99fac7dce7e0478eba7d4893029f7aa5c
AntonOshurek <oshurekweb@gmail.com>
2021-10-13 17:26:55 +0200

    get json for list

--- source/scripts/blocks/valute-list.js
+++ source/scripts/blocks/valute-list.js
@@ -1,3 +1,26 @@
 export default function valuteList() {
-  
+
+  fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+  .then(response => response.json())
+  .then(json => getValute(json))
+
+  const getValute = (val) => {
+    let valute = val.Valute;
+    console.log(valute)
+
+    showValute(valute);
+  }
+
+  const showValute = (valute) => {
+    const valuteList = document.querySelector('.valute__list');
+    const valuteItems = [];
+
+    for(let key in valute) {
+      valuteItems.push(valute[key])
+    }
+
+    console.log(valuteItems)
+
+  }
+
 }
