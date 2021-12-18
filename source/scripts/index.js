c79f8e594ca7776869ee142f18205fa8321b5f1e
AntonOshurek <oshurekweb@gmail.com>
2021-12-18 12:50:24 +0100

    time correction

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,10 @@
 //default
 import converter from './blocks/coverter';
 import clock from './blocks/clock';
-import valuteList from './blocks/valute-list'
 
 
 window.addEventListener('DOMContentLoaded', () => {
   converter();
   clock();
-  valuteList();
 });
 
